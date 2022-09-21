b969f20ceb9984dc745105e01f80de7b19b5e076
Jeffrey-Roberts <jeffrey.roberts@my.simpson.edu>
2022-09-21 10:36:45 -0500

    proof of concept

---
+++ ProofOfConcept.py
@@ -0,0 +1,24 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 150)
+
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+dict = ['dog', 'cat', 'house']
+
+for word in dict:
+    engine.say(word)
+    engine.runAndWait()
+
+    answer = input()
+    if answer == word:
+        print("CORRECT")
+    else:
+        print("WRONG")
+
+engine.stop()
+
