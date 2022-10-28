df990e5795bd7c62f4c355082a4b87bb8ee945c5
SMMccoy <85355929+SMMccoy@users.noreply.github.com>
2022-10-28 10:27:58 -0500

    edited wordlist

--- mvc_example/WordList.py
+++ mvc_example/WordList.py
@@ -1,7 +1,4 @@
-word_list = [
-    "Hello",
-    "Testing",
-    "One",
-    "Two",
-    "Three"
-]
+import json
+
+word_list_read = open("mvc\demofile4.json","r")
+word_list = json.loads(word_list_read.read())
