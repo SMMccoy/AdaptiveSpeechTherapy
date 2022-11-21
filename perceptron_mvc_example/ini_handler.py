ddcc6d80eb1a42fb4518ff3b03af175bafa74409
Ean Warrick <eanwarrick@gmail.com>
2022-11-21 11:00:02 -0600

    fixed saving

--- perceptron_mvc_example/ini_handler.py
+++ perceptron_mvc_example/ini_handler.py
@@ -2,15 +2,15 @@ import json
 
 class ini():
     def __init__(self) -> None:
-        ini_file_read = open("~/ini.py","r")
-        ini_file_read.close()
+        ini_file_read = open("ini.json","r")
         self.ini_dict = json.loads(ini_file_read.read())
+        ini_file_read.close()
     def getLevel(self):
         return self.ini_dict["level"]
     def setLevel(self,level):
         self.ini_dict["level"] = level
     def endSession(self):
-        ini_file_write = open("~/ini.py","w")
+        ini_file_write = open("ini.json","w")
         ini_file_write.write(json.dumps(self.ini_dict))
         ini_file_write.close()
         
